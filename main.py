4c53ecd40d45322b95a2c6fe562a0e7cf27f8bc9
Benedict Jerome Mtsitsi <mtsitsibenedictjerome@gmail.com>
2026-08-27 05:17:29 +0200

    Change greeting on master branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from colorama import Fore
 
-print(Fore.GREEN + "Hello, Jerome!")
+print(Fore.GREEN + "Hello from master branch, Jerome!")
 print(Fore.RED + "Welcome to my Python project, Jerome!")
 print(Fore.BLUE + "This is my feature branch!")
 print(Fore.YELLOW + "This is my new feature!")
