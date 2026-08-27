749c8ef1f18c235d06c479ea9f482cdea2ec5880
Benedict Jerome Mtsitsi <mtsitsibenedictjerome@gmail.com>
2026-08-27 05:47:13 +0200

    Test HEAD movement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+
 from colorama import 
 
 print(Fore.GREEN + "Hello from master branch, Jerome!")
 print(Fore.RED + "Welcome to my Python project, Jerome!")
 print(Fore.BLUE + "This is my feature branch!")
 print(Fore.YELLOW + "This is my new feature!")
+print("Testing HEAD movement!")
