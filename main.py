97a408720c027bfe9b1779aa1e567a4bf85229aa
Benedict Jerome Mtsitsi <mtsitsibenedictjerome@gmail.com>
2026-08-27 05:06:00 +0200

    Add new feature message

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from colorama import Fore
 print(Fore.GREEN + "Hello, Jerome!")
 print(Fore.RED + "Welcome to my Python project, Jerome!")
 print(Fore.BLUE + "This is my feature branch!")
+print(Fore.YELLOW + "This is my new feature!")
