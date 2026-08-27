bdda324a8597d39113c90b1e44f2e77ca972b8d1
Benedict Jerome Mtsitsi <mtsitsibenedictjerome@gmail.com>
2026-08-27 13:11:59 +0200

    Add learning message

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
-from colorama import 
+from colorama import Fore
 
 print(Fore.GREEN + "Hello from master branch, Jerome!")
 print(Fore.RED + "Welcome to my Python project, Jerome!")
 print(Fore.BLUE + "This is my feature branch!")
 print(Fore.YELLOW + "This is my new feature!")
-print("Testing HEAD movement!")
+
+print("I am learning Python and Git!")
