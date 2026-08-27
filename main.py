8bb93d19d414eefae9c696fc7013ef2b198c1670
Benedict Jerome Mtsitsi <mtsitsibenedictjerome@gmail.com>
2026-08-27 05:14:19 +0200

    Change greeting on conflict branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from colorama import Fore
+from colorama import 
 
-print(Fore.GREEN + "Hello, Jerome!")
+print(Fore.GREEN + "Hello from conflict branch, Jerome!")
 print(Fore.RED + "Welcome to my Python project, Jerome!")
 print(Fore.BLUE + "This is my feature branch!")
 print(Fore.YELLOW + "This is my new feature!")
