de5de05ce67890cde4e9b62bac07e8e2fe662c26
Benedict Jerome Mtsitsi <mtsitsibenedictjerome@gmail.com>
2026-08-30 08:13:22 +0200

    Add cherry-pick feature

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from colorama import Fore
-
-print(Fore.GREEN + "Hello from master branch, Jerome!")
-print(Fore.RED + "Welcome to my Python project, Jerome!")
-print(Fore.BLUE + "This is my feature branch!")
-print(Fore.YELLOW + "This is my new feature!")
-
-print("I am learning Python and Git!")
-print("This feature was created on the feature branch!")
+print("Hello, Git!")
 print("Learning staged changes!")
 print("Version 1.1 feature added!")
 print("Remote branch feature")
 print("Pull request demo feature")
+print("Rebase demo feature")
+print("Cherry-pick feature")
